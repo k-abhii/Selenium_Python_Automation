75f98d5a06c5f92a5d99269f50bf0e68a2c949cb
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-12-19 22:01:46 +0530

    time.sleep(2)

--- waitDemo.py
+++ waitDemo.py
@@ -12,3 +12,8 @@ print(driver.title)
 print(driver.current_url)
 driver.find_element(By.CSS_SELECTOR,".search-keyword").send_keys("ber")
 time.sleep(2)
+results = driver.find_elements(By.XPATH,"//div[@class='products']/div")
+count = len(results)
+print(count)
+assert count == 3
+# for result in results:
